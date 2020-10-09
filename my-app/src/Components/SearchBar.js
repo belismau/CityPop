6cb78dace32c9ed3c1339b23e8e9f06470abc813
Bel <belismau@gmail.com>
2020-10-09 21:35:15 +0200

    Added function on click (on input)

--- my-app/src/Components/SearchBar.js
+++ my-app/src/Components/SearchBar.js
@@ -2,6 +2,32 @@ import React from 'react'
 import './SearchBar.css'
 
 class SearchBar extends React.Component {
+    constructor() {
+        super()
+
+        this.state = {
+            input: null,
+            searched: false,
+            searchedValue: null
+        }
+
+        this.getValue = this.getValue.bind(this)
+        this.setSearchValue = this.setSearchValue.bind(this)
+    }
+
+    getValue(e) {
+        this.setState({
+            input: e.target.value
+        })
+    }
+
+    setSearchValue() {
+        this.setState({
+            searched: true,
+            searchedValue: this.state.input
+        })
+    }
+
     render() {
         return (
             <main className="SearchBar">
@@ -9,10 +35,12 @@ class SearchBar extends React.Component {
                     <div>
                         <input 
                             type="text"
+                            onChange={this.getValue}
                             placeholder={this.props.newPlaceholder}
                         />
                         <div 
-                            className="SearchIcon">
+                            className="SearchIcon"
+                            onClick={this.setSearchValue}>
                             <img
                                 src="https://secure.webtoolhub.com/static/resources/icons/set103/ce6535a5.png"
                                 alt="Search"
@@ -20,6 +48,13 @@ class SearchBar extends React.Component {
                         </div>
                     </div>
                 </div>
+
+                {this.state.searched ?
+                    <p> {this.state.searchedValue}</p> 
+                :
+                    null
+                }
+
             </main>
         )
     }
