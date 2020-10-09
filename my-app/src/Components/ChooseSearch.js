0c4d2605e1bc75fee4bb9087939b9d87284155ad
Bel <belismau@gmail.com>
2020-10-09 21:18:25 +0200

    Added searchByCity() & searchByCountry()

--- my-app/src/Components/ChooseSearch.js
+++ my-app/src/Components/ChooseSearch.js
@@ -2,17 +2,53 @@ import React from 'react'
 import './ChooseSearch.css'
 
 class ChooseSearch extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            chosen: false,
+            placeholder: '',
+            countrySearch: false
+        }
+
+        this.searchByCity = this.searchByCity.bind(this)
+        this.searchByCountry = this.searchByCountry.bind(this)
+    }
+
+    searchByCity() {
+        this.setState({
+            chosen: true,
+            placeholder: 'Enter a city...',
+            countrySearch: false
+        })
+    }
+
+    searchByCountry() {
+        this.setState({
+            chosen: true,
+            placeholder: 'Enter a country...',
+            countrySearch: true
+        })
+    }
+    
     render() {
-        return (
-            <div className="ChooseSearch">
-                <div onClick={this.searchByCity}> 
-                    <p> Search by city </p>
+        if (this.state.chosen === false) {
+            return (
+                <div className="ChooseSearch">
+                    <div onClick={this.searchByCity}> 
+                        <p> Search by city </p>
+                    </div>
+                    <div onClick={this.searchByCountry}> 
+                        <p> Search by country </p>
+                    </div>
                 </div>
-                <div onClick={this.searchByCountry}> 
-                    <p> Search by country </p>
+            )
+        } else {
+            return (
+                <div>
+                    <input placeholder={this.state.placeholder}></input>
                 </div>
-            </div>
-        )
+            )
+        }
     }
 }
 
