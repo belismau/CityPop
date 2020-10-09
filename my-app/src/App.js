0bbf9cbf4f537666082189d70342867eb626f6ab
Bel <belismau@gmail.com>
2020-10-09 15:23:46 +0200

    Created App.js

---
+++ my-app/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class App extends React.Component {
+    render() {
+        return (
+          <div> This is App.js </div>
+        )
+    }
+}
+
+export default App;
