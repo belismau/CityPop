07fbe51755f7b7f31d7781deb58d93e1f6103f4b
Bel <belismau@gmail.com>
2020-10-10 11:36:04 +0200

    Added ability to search on city

---
+++ my-app/src/Components/PresentInfo.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './PresentInfo.css'
+
+class PresentInfo extends React.Component {
+    render() {
+        if (this.props.noInfo) {
+            return (
+                <div className="PresentInfo">
+                    <h1> Inga resultat för
+                        <span> "{this.props.userInput}" </span>
+                    </h1>
+                </div>
+            )
+        } else {
+            return (
+                <div className="PresentInfo">
+                    <h1> Resultat för
+                        <span> "{this.props.userInput}" </span>
+                    </h1>
+                    {this.props.cities.map(city => (
+                        <h3 key={city}> {city}, {this.props.countryName} </h3>
+                    ))}
+                    {this.props.population.map(city => (
+                        <div id="cityPopulation" key={city}>
+                            <p key={city}> {city} </p>
+                        </div>
+                    ))}
+                </div>
+            )
+        }
+    }
+}
+
+export default PresentInfo;
--- my-app/src/Components/SearchBar.js
+++ my-app/src/Components/SearchBar.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PresentInfo from './PresentInfo'
 import './SearchBar.css'
 
 class SearchBar extends React.Component {
@@ -24,7 +25,57 @@ class SearchBar extends React.Component {
     setSearchValue() {
         this.setState({
             searched: true,
-            searchedValue: this.state.input
+            searchedValue: this.state.input,
+            population: [],
+            cities: [],
+            noInfo: false,
+            countryName: '',
+            loading: false
+        })
+
+        this.fetchCityInfo()
+    }
+
+
+    fetchCityInfo() {
+        fetch('http://api.geonames.org/searchJSON?&q=' + this.state.input + '&maxRows=1&username=spopre2')
+        .then(res => res.json())
+        .then((data) => {
+
+            let subroot = data['geonames'][0]
+
+            if ((subroot.fclName).substring(0, 4) === 'city') {
+                this.setStateForPopAndCity(
+                    subroot.population, 
+                    subroot.toponymName
+                )
+                
+                this.setState({
+                    countryName: subroot.countryName
+                })
+            } else {
+                this.setStateForNoInfo(true)
+            }
+        })
+        .catch(console.log)
+    }
+
+    setStateForNoInfo(boolean) {
+        this.setState({
+            noInfo: boolean
+        })
+    }
+
+    setStateForPopAndCity(varPopulation, varCities) {
+        this.setState({
+            population: [
+                ...this.state.population,
+                varPopulation
+            ],
+            cities: [
+                ...this.state.cities,
+                varCities
+            ]
         })
     }
 
@@ -50,8 +101,13 @@ class SearchBar extends React.Component {
                 </div>
 
                 {this.state.searched ?
-                    <p> {this.state.searchedValue}</p> 
-                :
+                    <PresentInfo
+                        userInput={this.state.searchedValue}
+                        population={this.state.population}
+                        cities={this.state.cities}
+                        noInfo={this.state.noInfo}
+                        countryName={this.state.countryName}
+                    /> :
                     null
                 }
 
